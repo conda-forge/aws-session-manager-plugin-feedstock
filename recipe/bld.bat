2a1a40ba767087ae8d3909fc971d5a769314936c
Simon Li <orpheus+devel@gmail.com>
2022-10-15 21:56:16 +0100

    go-licenses: `GO111MODULE=auto`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,7 @@ COPY %SRC_DIR%\bin\windows_amd64\ssmcli.exe %PREFIX%\bin
 COPY %SRC_DIR%\bin\windows_amd64_plugin\session-manager-plugin.exe %PREFIX%\bin
 
 SET GOPATH=%CD%\vendor;%CD%\build\private
+set GO111MODULE=auto
 go-licenses save .\src\sessionmanagerplugin-main\ .\src\ssmcli-main\ --save_path=.\license-files
 
 IF NOT EXIST license-files\github.com\ EXIT \b 1
