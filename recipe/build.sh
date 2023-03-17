e61af1c1256859e51b58a1638aceded49d455234
Simon Li <orpheus+devel@gmail.com>
2023-03-17 19:36:55 +0000

    Update build.sh for arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,18 +2,41 @@
 set -eu
 
 
-if [[ "${target_platform}" == osx-* ]]; then
+if [[ "${target_platform}" == osx-64 ]]; then
   MACHINE=darwin
-else
+  MACHINE_ARCH=amd64
+  MAKE_TARGET=build-${MACHINE}-${MACHINE_ARCH}
+elif [[ "${target_platform}" == osx-arm64 ]]; then
+  MACHINE=darwin
+  MACHINE_ARCH=arm64
+  MAKE_TARGET=build-${MACHINE}-${MACHINE_ARCH}
+elif [[ "${target_platform}" == linux-64 ]]; then
   MACHINE=linux
+  MACHINE_ARCH=amd64
+  MAKE_TARGET=build-${MACHINE}-${MACHINE_ARCH}
+elif [[ "${target_platform}" == linux-aarch64 ]]; then
+  MACHINE=linux
+  MACHINE_ARCH=arm64
+  # Makefile has inconsistently named build-* targets
+  MAKE_TARGET=build-${MACHINE_ARCH}
+else
+  echo "Unsupported platform: ${target_platform}"
+  exit 1
 fi
-make "build-${MACHINE}-amd${ARCH}"
+make "build-${MACHINE}-${MACHINE_ARCH}"
 
 mkdir -p $PREFIX/bin
 cp \
-  "${SRC_DIR}/bin/${MACHINE}_amd${ARCH}/ssmcli" \
-  "${SRC_DIR}/bin/${MACHINE}_amd${ARCH}_plugin/session-manager-plugin" \
+  "${SRC_DIR}/bin/${MACHINE}_${MACHINE_ARCH}_plugin/session-manager-plugin" \
   "$PREFIX/bin"
+
+# ssmcli is missing on linux arm64
+if [[ "${MAKE_TARGET}" != build-arm64 ]]; then
+cp \
+  "${SRC_DIR}/bin/${MACHINE}_${MACHINE_ARCH}/ssmcli" \
+  "$PREFIX/bin"
+fi
+
 
 # Some vendor directories don't have a licence file
 export GOPATH="$( pwd )/vendor"
